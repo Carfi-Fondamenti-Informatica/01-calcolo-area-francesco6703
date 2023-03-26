5e824c509536e512350ea1cc47cde4e670416f83
francesco6703 <127511152+francesco6703@users.noreply.github.com>
2023-03-26 17:01:34 +0200

    Create calcolo area.cpp

---
+++ calcolo area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+float a, b, c, Atriangolo, Aquadrato, Arettangolo, Atrapezio;
+cin >> a >> b >> c;
+
+Atriangolo=(a*b)/2;
+Aquadrato=a*a;
+Arettangolo=a*b;
+Atrapezio=((a+b)*c)/2;
+
+cout << Atriangolo << "\n" << Aquadrato << "\n" << Arettangolo << "\n" << Atrapezio;
+return0;
+}
